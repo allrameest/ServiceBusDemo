32650c78253699ee77ef1ffe187637b0cb277b2b
Erik Juhlin <erik@juhlin.nu>
2013-05-21 22:24:55 +0200

    OrderSaga tests

---
+++ Order/Tests/OrderSagaTests.cs
@@ -0,0 +1,43 @@
+﻿using Backend;
+using FakeItEasy;
+using Messages;
+using NUnit.Framework;
+using Rhino.ServiceBus;
+using SharpTestsEx;
+
+namespace Tests
+{
+    [TestFixture]
+    public class OrderSagaTests
+    {
+        [Test]
+        public void PaymentCompleted_WhenArticlesNotReserved_DeliverArticles()
+        {
+            var bus = A.Fake<IServiceBus>();
+            var orderSaga = new OrderSaga(bus);
+            orderSaga.State.ArticlesReserved = false;
+
+            orderSaga.Consume(new PaymentCompleted());
+
+            orderSaga.IsCompleted.Should().Be.False();
+            orderSaga.State.PaymentCompleted.Should().Be.True();
+            A.CallTo(() => bus.Send(A<object>.That.IsInstanceOf(typeof(DeliverArticles)))).MustNotHaveHappened();
+            A.CallTo(() => bus.Send(A<object>.That.IsInstanceOf(typeof(SendConfirmationEmail)))).MustNotHaveHappened();
+        }
+
+        [Test]
+        public void PaymentCompleted_WhenArticlesReserved_DeliverArticles()
+        {
+            var bus = A.Fake<IServiceBus>();
+            var orderSaga = new OrderSaga(bus);
+            orderSaga.State.ArticlesReserved = true;
+
+            orderSaga.Consume(new PaymentCompleted());
+
+            orderSaga.IsCompleted.Should().Be.True();
+            orderSaga.State.PaymentCompleted.Should().Be.True();
+            A.CallTo(() => bus.Send(A<object>.That.IsInstanceOf(typeof(DeliverArticles)))).MustHaveHappened();
+            A.CallTo(() => bus.Send(A<object>.That.IsInstanceOf(typeof(SendConfirmationEmail)))).MustHaveHappened();
+        }
+    }
+}
